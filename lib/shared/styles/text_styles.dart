18e636f52fd0d22febaa34940715efab5e952526
vamgarcia <victor.garcia@celuweb.com>
2022-10-04 16:42:23 -0500

    create shared, colors, styles, util, fonts and res folders and config pubspect

---
+++ lib/shared/styles/text_styles.dart
@@ -0,0 +1,145 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class TextStyles {
+  static TextStyle largeStyle(
+      {Color color = Colors.black, bool isBold = false}) {
+    return TextStyle(
+      color: color,
+      fontWeight: isBold ? FontWeight.bold : FontWeight.normal,
+      fontFamily: 'OpenSans',
+      fontSize: Get.textScaleFactor * 34,
+    );
+  }
+
+  static TextStyle titleStyle(
+      {Color color = Colors.black, bool isBold = false}) {
+    return TextStyle(
+      color: color,
+      fontWeight: isBold ? FontWeight.bold : FontWeight.normal,
+      fontFamily: 'OpenSans',
+      fontSize: Get.textScaleFactor * 30,
+    );
+  }
+
+  static TextStyle title2Style(
+      {Color color = Colors.black, bool isBold = false}) {
+    return TextStyle(
+      color: color,
+      fontWeight: isBold ? FontWeight.bold : FontWeight.normal,
+      fontFamily: 'OpenSans',
+      fontSize: Get.textScaleFactor * 28,
+    );
+  }
+
+  static TextStyle title3Style(
+      {Color color = Colors.black, bool isBold = false}) {
+    return TextStyle(
+      color: color,
+      fontWeight: isBold ? FontWeight.bold : FontWeight.normal,
+      fontFamily: 'OpenSans',
+      fontSize: Get.textScaleFactor * 26,
+    );
+  }
+
+  static TextStyle subTitleStyle(
+      {Color color = Colors.black, bool isBold = false}) {
+    return TextStyle(
+      color: color,
+      fontWeight: isBold ? FontWeight.bold : FontWeight.normal,
+      fontFamily: 'OpenSans',
+      fontSize: Get.textScaleFactor * 22,
+    );
+  }
+
+  static TextStyle subTitle2Style(
+      {Color color = Colors.black, bool isBold = false}) {
+    return TextStyle(
+      color: color,
+      fontWeight: isBold ? FontWeight.bold : FontWeight.normal,
+      fontFamily: 'OpenSans',
+      fontSize: Get.textScaleFactor * 20,
+    );
+  }
+
+  static TextStyle subTitle3Style(
+      {Color color = Colors.black, bool isBold = false}) {
+    return TextStyle(
+      color: color,
+      fontWeight: isBold ? FontWeight.bold : FontWeight.normal,
+      fontFamily: 'OpenSans',
+      fontSize: Get.textScaleFactor * 18,
+    );
+  }
+
+  static TextStyle headlineStyle(
+      {Color color = Colors.black, bool isBold = false}) {
+    return TextStyle(
+      color: color,
+      fontWeight: isBold ? FontWeight.bold : FontWeight.normal,
+      fontFamily: 'OpenSans',
+      fontSize: Get.textScaleFactor * 16,
+    );
+  }
+
+  static TextStyle subHeadLineUnderLineStyle(
+      {Color color = Colors.black, bool isBold = false}) {
+    return TextStyle(
+      decoration: TextDecoration.underline,
+      color: color,
+      fontWeight: isBold ? FontWeight.bold : FontWeight.normal,
+      fontFamily: 'OpenSans',
+      fontSize: Get.textScaleFactor * 14,
+    );
+  }
+
+  static TextStyle subHeadLineStyle(
+      {Color color = Colors.black, bool isBold = false}) {
+    return TextStyle(
+      color: color,
+      fontWeight: isBold ? FontWeight.bold : FontWeight.normal,
+      fontFamily: 'OpenSans',
+      fontSize: Get.textScaleFactor * 14,
+    );
+  }
+
+  static TextStyle captionStyle(
+      {Color color = Colors.black, bool isBold = false}) {
+    return TextStyle(
+      color: color,
+      fontWeight: isBold ? FontWeight.bold : FontWeight.normal,
+      fontFamily: 'OpenSans',
+      fontSize: Get.textScaleFactor * 12,
+    );
+  }
+
+  static TextStyle caption2Style(
+      {Color color = Colors.black, bool isBold = false}) {
+    return TextStyle(
+      color: color,
+      fontWeight: isBold ? FontWeight.bold : FontWeight.normal,
+      fontFamily: 'OpenSans',
+      fontSize: Get.textScaleFactor * 11,
+    );
+  }
+
+  static TextStyle caption3Style(
+      {Color color = Colors.black, bool isBold = false}) {
+    return TextStyle(
+      color: color,
+      fontWeight: isBold ? FontWeight.bold : FontWeight.normal,
+      fontFamily: 'OpenSans',
+      fontSize: Get.textScaleFactor * 10,
+    );
+  }
+
+  static TextStyle bodyStyle(
+      {Color color = Colors.black, bool isBold = false}) {
+    return TextStyle(
+      color: color,
+      fontWeight: isBold ? FontWeight.bold : FontWeight.normal,
+      fontFamily: 'OpenSans',
+      fontSize: Get.textScaleFactor * 18,
+    );
+  }
+}
