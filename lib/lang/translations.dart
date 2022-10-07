b436b9f958079c0f03f99fec00ca6622c7470d70
vamgarcia <victor.garcia@celuweb.com>
2022-10-07 11:53:52 -0500

    tranalatiosn

--- lib/lang/translations.dart
+++ lib/lang/translations.dart
@@ -1,1 +1,21 @@
-class AppTranslations {}
+import 'package:get/get.dart';
+import 'messages.dart';
+
+class AppTranslations extends Translations {
+  @override
+  Map<String, Map<String, String>> get keys => allKeys();
+
+  Map<String, Map<String, String>> allKeys() {
+    Iterable<Map<String, String>> generalTranslations =
+        Messages().keys.values.where((element) => element.isNotEmpty);
+
+    Map<String, String> enMap = {};
+    enMap.addAll(generalTranslations.first);
+
+    Map<String, String> esMap = {};
+    esMap.addAll(generalTranslations.last);
+
+    var map = {'en': enMap, 'es': esMap};
+    return map;
+  }
+}
