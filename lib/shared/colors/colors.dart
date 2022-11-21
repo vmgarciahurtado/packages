de8e18d948ce083e46fecb92bbb03bddbb7b9485
vamgarcia <victor.garcia@celuweb.com>
2022-11-21 13:22:24 -0500

    restore default colors

--- lib/shared/colors/colors.dart
+++ lib/shared/colors/colors.dart
@@ -4,8 +4,8 @@ import 'hex_color.dart';
 class Colores {
   Colores();
 
-  static var primaryColor; //= HexColor("#002341");
-  static var secondaryColor; // = HexColor("#004139");
+  static var primaryColor = HexColor("#002341");
+  static var secondaryColor = HexColor("#004139");
   static final primaryGray = HexColor("#656565");
   static final secondaryGray = HexColor("#ACACAC");
   static final dangerColor = Colors.red.shade400;
